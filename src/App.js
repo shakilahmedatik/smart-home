bb28d748ff1756d0043989c6e0f0952dc73c7b0d
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-10-08 15:37:07 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-800">Welcome to Smart Home</h1>
+    </div>
+  );
+}
+
+export default App;
